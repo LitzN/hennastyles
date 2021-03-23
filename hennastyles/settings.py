355007c4796cb000460476cba6aa8b7c0589c6b2
LitzN <alitzianedyalkova@hotmail.co.uk>
2021-03-23 22:27:27 +0000

    Added debug settings and storage settings

--- hennastyles/settings.py
+++ hennastyles/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['hennastyles.herokuapp.com', 'localhost']
 
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'profiles',
     'community',
-    'django-storages'
+    'storages',
 ]
 
 MIDDLEWARE = [
