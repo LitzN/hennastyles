bba36ac4ed2cba1d0e1851472083e9903f7e86ce
LitzN <alitzianedyalkova@hotmail.co.uk>
2021-03-24 23:31:53 +0000

    Fixed bug with checkout signals

--- checkout/views.py
+++ checkout/views.py
@@ -19,15 +19,15 @@ def cache_checkout_data(request):
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            "bag": json.dumps(request.session.get('bag', {})),
-            "save_info": request.POST.get('save_info'),
-            "username": request.user,
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, 'Sorry, your payment could not be \
             processed right now. Please try again later.')
-        return HttpResponse(content=e, status=404)
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -36,7 +36,6 @@ def checkout(request):
 
     if request.method == 'POST':
         bag = request.session.get('bag', {})
-
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
@@ -74,7 +73,7 @@ def checkout(request):
                                 quantity=quantity,
                                 product_colour=colour,
                             )
-                        order_line_item.save()
+                            order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your shopping bag wasn't found in our database.\
@@ -94,8 +93,8 @@ def checkout(request):
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
-        total = current_bag['grand_total']
-        stripe_total = round(total * 100)
+        grand_total = current_bag['grand_total']
+        stripe_total = round(grand_total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
