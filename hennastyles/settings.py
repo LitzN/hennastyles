ff368d2964fe458dde0eb3fb13a9987e8d429c8a
LitzN <alitzianedyalkova@hotmail.co.uk>
2021-03-31 10:18:24 +0000

    Final styling changes and fixing error with setting

--- hennastyles/settings.py
+++ hennastyles/settings.py
@@ -128,8 +128,7 @@ else:
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.\
-            UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
